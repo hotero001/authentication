89dc0c49ff93552243b2137d69ce439dab1a5462
Hector Otero <otero111@hotmail.com>
2015-03-22 19:14:09 -0700

    Rails app from rails tutorial

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+	enum role: [:user, :vip, :admin]
+	after_initialize :set_default_role, :if => :new_record?
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def set_default_role
+  	self.role ||= :user
+  end
+
+  devise :database_authenticatable, :registerable,
+  	:recoverable, :rememberable, :trackable, :validatable
+end
